aba67fd257e23e60c8d430f29f1f33a6e9d0596a
Murphy-hub <murphy.bela@gmail.com>
2022-08-09 16:53:32 +0800

    fix: version

--- initer/flags.go
+++ initer/flags.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2022-present The Ztalab Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package initer
 
 import (
@@ -13,12 +26,51 @@ import (
 	"github.com/ztalab/ZACA/core"
 	"github.com/ztalab/ZACA/core/config"
 	"github.com/ztalab/ZACA/pkg/influxdb"
+	"gopkg.in/yaml.v3"
 )
 
 const (
 	G_       = "IS"
 	ConfName = "conf"
 )
+
+func InitConfigs(c *cli.Context, configURL string) (core.Config, error) {
+	var conf config.IConfig
+	f, err := os.Open(configURL)
+	if err != nil {
+		return core.Config{}, err
+	}
+	if err = yaml.NewDecoder(f).Decode(&conf); err != nil {
+		return core.Config{}, err
+	}
+	if v := os.Getenv("IS_ENV"); v != "" {
+		conf.Debug = true
+	}
+	if v := os.Getenv("IS_SINGLECA_CONFIG_PATH"); v != "" {
+		conf.Singleca.ConfigPath = v
+	}
+	if v := os.Getenv("IS_MYSQL_DSN"); v != "" {
+		conf.Mysql.Dsn = v
+	}
+	if v := os.Getenv("IS_KEYMANAGER_UPPER_CA"); v != "" {
+		conf.Keymanager.UpperCa = strings.Split(v, " ")
+	}
+	// ref: https://github.com/golang-migrate/migrate/issues/313
+	if !strings.Contains(conf.Mysql.Dsn, "multiStatements") {
+		conf.Mysql.Dsn += "&multiStatements=true"
+	}
+
+	cfg, err := cfssl_config.LoadFile(conf.Singleca.ConfigPath)
+	if err != nil {
+		return core.Config{}, fmt.Errorf("cfssl configuration file %s Error: %s", conf.Singleca.ConfigPath, err)
+	}
+	cfg.Signing.Default.OCSP = conf.OCSPHost
+	conf.Singleca.CfsslConfig = cfg
+
+	return core.Config{
+		IConfig: conf,
+	}, nil
+}
 
 func parseConfigs(c *cli.Context) (core.Config, error) {
 	// Cmdline flags
--- main.go
+++ main.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2022-present The Ztalab Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -20,6 +33,13 @@ func main() {
 		newApiCmd(ctx),
 		newTlsCmd(ctx),
 		newOcspCmd(ctx),
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "conf",
+			Usage: "配置文件(.yaml)",
+			Value: "conf.yml",
+		},
 	}
 	app.Before = initer.Init
 	err := app.Run(os.Args)
